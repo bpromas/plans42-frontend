e880155477bf1d8968232708ed07b8f40171bd53
Bruno Romas <bpromas@gmail.com>
2026-05-19 17:17:58 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+
+  return (
+    <section>
+      <h1>Plans42</h1>
+      <h3>para as coisas que mais importam</h3>
+      <div>
+        <form action="post" className="flex flex-col">
+          <input type="text" />
+          <input type="text" />
+        </form>
+      </div>
+    </section>
+  )
+}
+
+export default App
